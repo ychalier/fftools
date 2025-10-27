3079769726e476de21df538d74191adbc51ad4a3
Yohan Chalier <yohan@chalier.fr>
2025-10-27 09:56:54 +0100

    added retime feature to blend-frames tool

--- fftools/tools/blend_frames.py
+++ fftools/tools/blend_frames.py
@@ -14,19 +14,31 @@ class BlendFrames(OneToOneTool):
             template: str,
             operation: str = "average",
             size: int = 3,
-            fixed: bool = False):
+            fixed: bool = False,
+            retime: bool = False):
         OneToOneTool.__init__(self, template)
         self.size = size
         self.opname = operation
         self.fixed = fixed
+        self.retime = retime
         self.operation = utils.getop(operation)
 
     @staticmethod
     def add_arguments(parser):
         OneToOneTool.add_arguments(parser)
-        parser.add_argument("-p", "--operation", type=str, help="Operation to blend the images together", default="average", choices=["average", "brighter", "darker", "sum", "difference", "weight1", "weight3", "weight5", "weight10", "random"])
-        parser.add_argument("-s", "--size", type=int, help="Moving-window size (in frames) for blending", default=3)
-        parser.add_argument("-f", "--fixed", action="store_true", help="Use fixed window instead of a rolling window")
+        parser.add_argument("-p", "--operation", type=str, default="average",
+            choices=["average", "brighter", "darker", "sum", "difference",
+                     "weight1", "weight3", "weight5", "weight10", "random"],
+            help="operation to blend the images together")
+        parser.add_argument("-s", "--size", type=int, default=3,
+            help="moving-window size (in frames) for blending")
+        parser.add_argument("-f", "--fixed", action="store_true",
+            help="use fixed window instead of a rolling window")
+        parser.add_argument("-r", "--retime", action="store_true",
+            help="when a fixed window is used (with --fixed flag), only "
+            "output one frame per input window, effectively decreasing the "
+            "input framerate; this may be used for creating artificial motion "
+            "blur from high framerate videoss")
 
     def process(self, input_file: utils.InputFile) -> pathlib.Path:
         import numpy
@@ -35,10 +47,16 @@ class BlendFrames(OneToOneTool):
             "size": self.size
         })
         with utils.VideoInput(input_file.path) as vin:
-            length = vin.length
-            if not self.fixed:
-                length += self.size - 1
-            with utils.VideoOutput(output_path, vin.width, vin.height, vin.framerate, length) as vout:
+            if self.fixed and self.retime:
+                length = vin.length // self.size
+                framerate = vin.framerate // self.size
+            elif self.fixed:
+                length = vin.length
+                framerate = vin.framerate
+            else:
+                length = vin.length + self.size - 1
+                framerate = vin.framerate
+            with utils.VideoOutput(output_path, vin.width, vin.height, framerate, length) as vout:
                 if self.fixed:
                     running = True
                     while running:
@@ -53,6 +71,8 @@ class BlendFrames(OneToOneTool):
                         out_frame = self.operation(numpy.array(frames))
                         for _ in frames:
                             vout.feed(out_frame)
+                            if self.retime:
+                                break
                 else:
                     frames = []
                     for frame in vin:
